cb6c7db722b8943607229d7a08307d410040ef53
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-14 09:46:28 +0200

    Add a countdown circle to the timer

--- src/components/PomodoroTimer.js
+++ src/components/PomodoroTimer.js
@@ -1,73 +1,69 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 function PomodoroTimer() {
-  const [minutes, setMinutes] = useState(25)
-  const [seconds, setSeconds] = useState(0)
   const [start, setStart] = useState(false)
+  const [key, setKey] = useState(0)
   const [session, setSession] = useState('focus')
   const [focusSessions, setFocusSessions] = useState(0)
+  const [duration, setDuration] = useState(25 * 60)
 
-  const handleStart = () => {
-    setStart(true)
-  }
-
-  const handleStop = () => {
-    setStart(false)
-  }
-
-  useEffect(() => {
-    let interval = null
+  const handleStartStop = () => {
     if (start) {
-      interval = setInterval(() => {
-        if (seconds > 0) {
-          setSeconds(seconds - 1)
-        }
-        if (seconds === 0) {
-          if (minutes === 0) {
-            if (session === 'focus') {
-              setFocusSessions(focusSessions + 1)
-              if (focusSessions === 4) {
-                setMinutes(15) // long break
-                setSession('long-break')
-              } else {
-                setMinutes(5) // short break
-                setSession('short-break')
-              }
-            } else {
-              setMinutes(25) // focus
-              if (session === 'long-break') {
-                setFocusSessions(0)
-              }
-              setSession('focus')
-            }
-          } else {
-            setMinutes(minutes - 1)
-            setSeconds(59)
-          }
-        }
-      }, 1000)
-    } else if (!start) {
-      clearInterval(interval)
+      setStart(false)
+    } else {
+      setStart(true)
+      setKey((prevKey) => prevKey + 1)
     }
-    return () => clearInterval(interval)
-  }, [minutes, seconds, start, session, focusSessions])
+  }
 
-  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes
-  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds
+  const timerProps = {
+    isPlaying: start,
+    key: key,
+    size: 120,
+    strokeWidth: 6,
+  }
+
+  const handleComplete = () => {
+    if (session === 'focus') {
+      setFocusSessions((prevFocusSessions) => prevFocusSessions + 1)
+      if (focusSessions === 3) {
+        setDuration(15 * 60) // long-break
+        setSession('long-break')
+      } else {
+        setDuration(5 * 60) // short-break
+        setSession('short-break')
+      }
+    } else {
+      setDuration(25 * 60)
+      if (session === 'long-break') {
+        setFocusSessions(0)
+      }
+      setSession('focus') // focus
+    }
+    setKey((prevKey) => prevKey + 1)
+    return [true, 1000] // repeat animation after 1 second delay
+  }
 
   return (
     <div className="pomodoro">
-      <div className="timer">
-        {timerMinutes}:{timerSeconds}
-      </div>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors={session === 'focus' ? [['#EF798A']] : [['#218380']]}
+        duration={duration}
+        onComplete={handleComplete}
+      >
+        {({ remainingTime }) => {
+          const minutes = Math.floor(remainingTime / 60)
+          const seconds = remainingTime % 60
+          return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+        }}
+      </CountdownCircleTimer>
+
       <div className="intervals">Interval {focusSessions + 1} of 5</div>
 
       <div className="buttons">
-        {!start ? (
-          <button onClick={handleStart}>Start</button>
-        ) : (
-          <button onClick={handleStop}>Stop</button>
-        )}
+        <button onClick={handleStartStop}>{!start ? 'Start' : 'Stop'}</button>
       </div>
     </div>
   )
