bfb94ed53c4f8c65461b039da06fac3b9acd5ad6
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-20 11:12:29 +0200

    Update manifest and files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { register } from './serviceWorker'
+
+register()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
