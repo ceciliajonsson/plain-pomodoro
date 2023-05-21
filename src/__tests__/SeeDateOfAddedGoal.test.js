e7711618abe5afc0dea8f16e49efa5f4171facfd
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-21 18:46:04 +0200

    Automated test for #22

---
+++ src/__tests__/SeeDateOfAddedGoal.test.js
@@ -0,0 +1,50 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import PomodoroTimer from '../components/PomodoroTimer'
+import { PomodoroContext } from '../contexts/PomodoroContext'
+import React from 'react'
+
+const mockPomodoroContext = {
+  settings: {
+    focusTime: 25 * 60,
+    shortBreak: 5 * 60,
+    longBreak: 15 * 60,
+    intervals: 4,
+  },
+}
+
+// Test if the date and time of the added goal is displayed
+test('date and time of added goal is displayed', async () => {
+  render(
+    <PomodoroContext.Provider value={mockPomodoroContext}>
+      <PomodoroTimer />
+    </PomodoroContext.Provider>
+  )
+
+  const goalInput = screen.getByRole('textbox', {
+    name: /Enter your goal for the session/i,
+  })
+  fireEvent.change(goalInput, {
+    target: { value: 'Test goal' },
+  })
+
+  const submitButton = screen.getByRole('button', { name: /Submit/i })
+  fireEvent.click(submitButton)
+
+  await waitFor(() => {
+    expect(screen.getByText(/Test goal/i)).toBeInTheDocument()
+  })
+
+  const date = new Date()
+  const day = date.toLocaleString('en-US', { day: '2-digit' })
+  const month = date.toLocaleString('en-US', { month: 'long' })
+  const year = date.toLocaleString('en-US', { year: 'numeric' })
+  const hour = date.getHours().toString().padStart(2, '0')
+  const minute = date.getMinutes().toString().padStart(2, '0')
+
+  const time = `${day} ${month} ${year}, ${hour}:${minute}`
+
+  await waitFor(() => {
+    expect(screen.getByText(time)).toBeInTheDocument()
+  })
+})
