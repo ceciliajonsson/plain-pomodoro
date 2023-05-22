c329ee2f930b4a1b21d2db1760a2d6309c34aeac
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-22 22:52:45 +0200

    Add about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PomodoroTimer from './components/PomodoroTimer'
 import { PomodoroProvider } from './contexts/PomodoroContext'
+import About from './components/About'
 
 function App() {
   return (
-    <PomodoroProvider>
-      <PomodoroTimer />
-    </PomodoroProvider>
+    <Router>
+      <PomodoroProvider>
+        <Routes>
+          <Route path="/" element={<PomodoroTimer />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </PomodoroProvider>
+    </Router>
   )
 }
 
