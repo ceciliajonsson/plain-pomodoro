622f32f0fb4d16db4681f72de813b572e24d76d0
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-24 23:08:22 +0200

    Fix settings info and footer links and design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-content">{<p>About</p>}</div>
-      <div className="footer-content">{<p>Download</p>}</div>
+      <div className="footer-content-holder">
+        <Link to={'/'} className="footer-content">
+          Home
+        </Link>{' '}
+        <div className="footer-divider">|</div>
+        <Link to={'/about'} className="footer-content">
+          About
+        </Link>
+      </div>
+      <div className="footer-content">© 2023 Plain Pomodoro</div>
     </div>
   )
 }
