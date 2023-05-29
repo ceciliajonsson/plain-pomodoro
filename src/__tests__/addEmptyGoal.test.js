75cea237c9b251ec4738875f0e78013aefb4b841
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-05-29 13:01:13 +0200

    Edit Test Case ID: AUT2.1

--- src/__tests__/addEmptyGoal.test.js
+++ src/__tests__/addEmptyGoal.test.js
@@ -2,7 +2,11 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import PomodoroTimer from '../components/PomodoroTimer'
 import { PomodoroContext } from '../contexts/PomodoroContext'
+import { MemoryRouter } from 'react-router-dom'
+
 import React from 'react'
+
+jest.mock('react-textarea-autosize', () => (props) => <textarea {...props} />)
 
 const mockPomodoroContext = {
   settings: {
@@ -18,7 +22,9 @@ const mockPomodoroContext = {
 test('user can not add an empty goal', async () => {
   render(
     <PomodoroContext.Provider value={mockPomodoroContext}>
-      <PomodoroTimer />
+      <MemoryRouter>
+        <PomodoroTimer />
+      </MemoryRouter>
     </PomodoroContext.Provider>
   )
 
